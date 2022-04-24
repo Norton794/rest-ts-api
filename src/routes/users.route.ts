fd1c9907a7ef64e2261fa70fdce2e43c2bc88b1c
Norton794 <nortonsantos79@gmail.com>
2022-04-24 14:19:15 -0300

    feat: http-status-codes

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,15 +1,17 @@
 import { Router, Request, Response, NextFunction } from "express";
-
+import {StatusCodes} from 'http-status-codes'
 const usersRoute = Router();
 
 usersRoute.get("/users", (req: Request, res: Response, next: NextFunction) => {
-    const users = [{userName: 'Norton'}]
-    res.status(200).json(users);
+  const users = [{ userName: "Norton" }];
+  res.status(StatusCodes.OK).json(users);
 });
 
-
-usersRoute.get("/users/:uuid", (req: Request, res: Response, next: NextFunction) => {
-    const {uuid} = req.params
-    res.status(200).json({uuid})
-});
-export default usersRoute
+usersRoute.get(
+  "/users/:uuid",
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const { uuid } = req.params;
+    res.status(StatusCodes.OK).json({ uuid });
+  }
+);
+export default usersRoute;
