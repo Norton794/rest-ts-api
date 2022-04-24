fbb917f76277553919c908557e616c2331463706
Norton794 <nortonsantos79@gmail.com>
2022-04-24 14:01:10 -0300

    feat: get users

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
+import usersRoute from './routes/users.route.ts'
 
 const app = express();
-app.get("/status", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ foi: 'Sucesso' });
-});
+
+app.use(usersRoute)
 
 app.listen(3010, ()=>{
     console.log("Aplicação na porta 3010")
