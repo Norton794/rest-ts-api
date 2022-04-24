b124c4e8d10906b1a382a7ef5642f5fff102b428
Norton794 <nortonsantos79@gmail.com>
2022-04-24 13:41:42 -0300

    usando ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('foi')
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foi: 'Sucesso' });
+});
+
+app.listen(3010, ()=>{
+    console.log("Aplicação na porta 3010")
+})
