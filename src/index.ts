226fcb62b83b1e4de741c9a626c750caa16f2856
Norton794 <nortonsantos79@gmail.com>
2022-04-30 11:30:10 -0300

    feat: post, put, delete e rota de status da aplicacao

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
-import usersRoute from './routes/users.route.ts'
-
+import usersRoute from "./routes/users.route.ts";
+import statusRoute from "./routes/status.route.ts";
 const app = express();
 
-app.use(usersRoute)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3010, ()=>{
-    console.log("Aplicação na porta 3010")
-})
+app.use(statusRoute);
+app.use(usersRoute);
+
+app.listen(3010, () => {
+  console.log("Aplicação na porta 3010");
+});
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -14,4 +14,29 @@ usersRoute.get(
     res.status(StatusCodes.OK).json({ uuid });
   }
 );
+
+usersRoute.post(
+    "/users",
+    (req: Request, res: Response, next: NextFunction) => {
+      const newUser = req.body;
+      console.log(req.body)
+      res.status(StatusCodes.CREATED).send(newUser);
+    }
+  );
+
+
+  usersRoute.put(
+    "/users/:uuid",
+    (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+      const { uuid } = req.params;
+      const modifiedUser = req.body;
+      modifiedUser.uuid = uuid;
+      res.status(StatusCodes.OK).send(modifiedUser);
+    }
+  );
+
+  usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction)=>{
+    const { uuid } = req.params;
+    res.sendStatus(StatusCodes.OK)
+  })
 export default usersRoute;
