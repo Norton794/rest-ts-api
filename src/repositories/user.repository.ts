003c38ce702c5c1765129e4767ab43b57f104de9
Norton794 <nortonsantos79@gmail.com>
2022-05-01 12:00:49 -0300

    refact: destructuring no SELECT de users

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -7,9 +7,9 @@ class UserRepository{
     async findAllUsers(): Promise<User[]>{
         const query = `SELECT uuid, username FROM application_user`
 
-       const result = await db.query<User>(query)
+       const {rows} = await db.query<User>(query)
         
-        return result.rows || [];
+        return rows || [];
     }
 }
 
